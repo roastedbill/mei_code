f8a73c38db64a56e7470840f1cfcdb3ccc384781
RenSheng Liu <liurs@garena.com>
2018-08-03 22:09:52 +0800

    Upd: adding edited music files and api to get mp3 file

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ Web IDE for peace and love.
 1. `npm i`
 2. `npm start`
 3. visit http://localhost:6075/
+4. pip install ./music_suggestion/requirements.txt
+5. export FLASK_APP=api.py
+6. flask run (:5000 by default)
--- music_suggestion/api.py
+++ music_suggestion/api.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask import send_from_directory
 import music_suggestion
+
 
 app = Flask(__name__)
 app.config.setdefault('WTF_CSRF_METHODS', ['POST', 'PUT', 'PATCH'])
@@ -10,3 +12,8 @@ def get_music_suggestion(text):
 	emotion = music_suggestion.get_emotion(text)
 	print 'get_music_suggestion: %s -> %s' % (text, emotion)
 	return emotion
+
+
+@app.route('/music/<path:filename>')
+def download_music(filename):
+	return send_from_directory('music', filename)
